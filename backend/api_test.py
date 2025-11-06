c9e8a32d1c2f1dd0a4c27c159ec1c696a60938fa
Gio G <gianpaolo.girasoli@uconn.edu>
2025-11-06 17:28:18 -0500

    all GET tests

--- backend/api_test.py
+++ backend/api_test.py
@@ -23,8 +23,7 @@ def client():
 
     app.testing = True
 
-    with app.test_client() as testClient:
-        yield testClient
+    yield app.test_client()
 
     if os.path.exists(TEST_DB): os.remove(TEST_DB) #remove test db
 
@@ -42,11 +41,17 @@ def test_create_trip(client):
         "isFormClosed": 0
     }
 
-    print(test_values)
+    #Test complete addition
     testCreate = client.post('/trips/create', json=test_values)
-    print(testCreate.json)
     assert testCreate.status_code == 201
     assert testCreate.json['trip']['tripLeader'] == "Jerma"
+
+    #Test incomplete addition
+    # test_values = {"tripName": "Nothing else here!"}
+
+    # testCreate = client.post('/trips/create', json=test_values)
+    # assert testCreate.status_code == 400
+
 
 def test_get_all_trips(client):
     """Test get all trips"""
@@ -57,3 +62,13 @@ def test_get_all_trips(client):
     assert response.json['count'] == 3
 
     assert len(response.json['trips']) == 3
+
+def test_get_current_trips(client):
+    """Test get current trips"""
+
+    response = client.get('/trips')
+
+    assert response.status_code == 200
+    assert response.json['count'] == 2
+
+    assert len(response.json['trips']) == 2
