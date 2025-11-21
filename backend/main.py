eb544ebd973cd26bd3f04a20fb294c3217430a11
gio <gianpaolo.girasoli@uconn.edu>
2025-11-21 14:33:43 -0500

    test db

--- backend/main.py
+++ backend/main.py
@@ -6,8 +6,11 @@ Initializes Flask app
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
+from init_db import CreateTables, InsertTestData
 # API
 from routes.trip_routes import trips_bp
+
+TEST_DB = 'ucoc.db'
 
 app = Flask(__name__)
 app.secret_key = 'iminsecure'
@@ -21,5 +24,14 @@ app.register_blueprint(trips_bp)
 def index():
     return "<h1>Your baby thinks that people can't change</h1>"
 
+def initializeDB():
+    if os.path.exists(TEST_DB): os.remove(TEST_DB)
+
+    conn = sqlite3.connect(TEST_DB)
+    CreateTables(conn)
+    InsertTestData(conn)
+    conn.close()
+
 if __name__ == '__main__':
+    initializeDB()
     app.run(host="0.0.0.0", port=5000)
